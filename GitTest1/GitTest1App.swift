cc9757a9d4aaa3b80f3c7ae2f99743b6763c1e3b
Sneha Kalariya <sneha.kalaria@gmail.com>
2023-09-19 18:18:02 -0700

    Initial Commit

---
+++ GitTest1/GitTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  GitTest1App.swift
+//  GitTest1
+//
+//  Created by Sneha Kalariya on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct GitTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
